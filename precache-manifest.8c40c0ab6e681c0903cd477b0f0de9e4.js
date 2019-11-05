2376649d8ba23d24ea5b8745862b53271cc1c5cc
pastorlalit <pastorlalit1989@gmail.com>
2019-11-05 23:14:37 +0530

    Updates

---
+++ precache-manifest.8c40c0ab6e681c0903cd477b0f0de9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba56fcb3ad5e386ee5d7ed248024db4",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "a3627cbc2d4d30ddea16",
+    "url": "/deploy-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "57cf0757c76213b1401d",
+    "url": "/deploy-react-app/static/js/2.47a91662.chunk.js"
+  },
+  {
+    "revision": "a3627cbc2d4d30ddea16",
+    "url": "/deploy-react-app/static/js/main.e37071ee.chunk.js"
+  },
+  {
+    "revision": "40eebbca7dab7ef75aab",
+    "url": "/deploy-react-app/static/js/runtime-main.c8ad58d2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
